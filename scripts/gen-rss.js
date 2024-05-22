12ef012ed82ff76f427c0dc3a465c554393236a6
Katon Surya Atmaja <katonatmaja@gmail.com>
2024-05-22 12:08:56 +0700

    Update RSS options

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml',
+    title: 'Katon.dev',
+    site_url: 'https://katon.dev',
+    feed_url: 'https://katon.dev/feed.xml',
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
